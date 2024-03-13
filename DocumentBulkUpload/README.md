2ac008315b1aa7623f6bb8c32181ebb6cf0bc3f1
srinivasansp <srinivasan.sp@invicara.com>
2024-03-13 09:16:18 +0530

    Document Bulk Upload Scripts and READ ME

---
+++ DocumentBulkUpload/README.md
@@ -0,0 +1,143 @@
+Overview
+
+The Bulk File/Document Upload with Attributes script is a powerful automation tool designed to streamline the process of uploading multiple files to a platform while associating specific attributes to each file.
+
+Features:
+
+1. Resumable File Uploads
+
+The script facilitates the resumable upload of large files, ensuring a smooth transfer and minimizing the risk of data loss during the upload process.
+
+2. Attribute Mapping
+
+Associates attributes with each file based on information provided in an Excel spreadsheet. Attributes are defined in the "Attribute_List" sheet of the spreadsheet.
+
+3. Custom Attribute Values
+
+Allows for the attachment of different attribute values to every file/document, providing flexibility in defining attributes on a per-file basis.
+
+4. Batch Processing
+
+Supports batch processing for cases where the number of files exceeds a predefined threshold. This prevents potential memory issues and ensures the successful upload of large sets of files.
+
+5. Status Tracking
+
+After uploading all files, we can easily track the status of each file/document. This includes information on which files were successfully uploaded , Which files were missing in the sheet and which files were fail to upload . 
+
+Prerequisites:
+
+Install & Access
+
+Command
+
+Path
+
+VS Code
+
+ 
+
+Link
+
+NPM & Node
+
+npm i node@14.19.0 
+
+npm & node
+
+NPM Registry setup
+
+ 
+
+Link
+
+GitHub Repository
+
+ 
+
+ClientApplicationConfig
+
+Twinit Extension
+
+ 
+
+Latest version
+
+Prepare an Excel spreadsheet with the following specifications:
+
+The spreadsheet must contain a sheet named "Attribute_List."
+
+The "Attribute_List" sheet should include a column named "File Name" to uniquely identify each file.
+
+Define additional columns for the attributes you wish to associate with each file.
+
+The script uses the information in this spreadsheet to map attributes to the corresponding files during the upload process.
+
+Attached Sample files for reference
+
+
+
+How to execute script:
+
+Details 
+
+Instructions 
+
+Screenshot & Files
+
+Run Script in Twinit Extension 
+
+Open VS code and Run Bulk upload script
+
+Open vs code , go to Digital Twin extension
+
+Select your project 
+
+Open script folder and select validation Script
+
+
+
+Select validation Script and Run Script
+
+Right click on Validation script
+
+Right Click on Validation Script and click on Run Script
+
+
+
+Select Script to Run
+
+Click on Run script 
+
+When you click on Run Script, script selection menu will open to select the script to run 
+
+Select bulk upload files script to run 
+
+
+
+Select Excel spreadsheet 
+
+Select Excel spreadsheet which have “Attribute_List” sheet 
+
+When we click on  bulk upload files option First popup will open to select the Excel spreadsheet which have “Attribute_List” sheet 
+
+
+
+Select multiple files to upload 
+
+Second popup will open to select the Files/Documents which we want to upload 
+
+We can select multiples Files/Documents 
+
+We can select any type of File/Document
+
+
+
+Upload status file 
+
+Download File/Document upload status file 
+
+When file/Document upload process will complete then we got the option to download status file 
+
+In status file we can track the status of Successfully Upload, Missing files in the sheet or Fail to uploaded Files/Documents 
+
+In status file we have three different sheets , Missing_File_In_Sheet, Successfully_Upload, Failed_To_Upload
---
+++ DocumentBulkUpload/scripts/BulkUpload.js
@@ -0,0 +1,211 @@
+let runnableScripts = [
+    { name: "Bulk Upload Files", script: "bulkUploadFileWithAttributes" }
+]
+
+let Validation = {
+    getRunnableScripts() {
+        return runnableScripts
+    },
+    async bulkUploadFileWithAttributes(input, libraries, ctx) {
+        let { PlatformApi, IafScriptEngine, UiUtils } = libraries
+        const proj = await PlatformApi.IafProj.getCurrent(ctx);
+        let iaf_ext_files_coll = await IafScriptEngine.getFileCollection({
+            _userType: "file_container",
+            _shortName: "Root Container"
+        }, ctx)
+        const containerObj = await PlatformApi.IafFile.getRootContainer(proj, ctx);
+        let xlsxFiles = await UiUtils.IafLocalFile.selectFiles({ multiple: false, accept: ".xlsx" });
+        let typeWorkbook = await UiUtils.IafDataPlugin.readXLSXFiles(xlsxFiles);
+        let wbJSON = UiUtils.IafDataPlugin.workbookToJSON(typeWorkbook[0]);
+        let iaf_dt_grid_data = wbJSON["Attribute_List"];
+        let data_as_objects = await UiUtils.IafDataPlugin.parseGridData({ gridData: iaf_dt_grid_data });
+
+        let selectedSheetData = {};
+        data_as_objects.forEach(function (item) {
+            var fileName = item['File Name'];
+            for (var key in item) {
+                if (item[key] === undefined) {
+                    item[key] = '';
+                }
+            }
+            delete item['File Name']
+            selectedSheetData[fileName] = item;
+        });
+
+        let selectFiles = await UiUtils.IafLocalFile.selectFiles({ multiple: true });
+        let missingFiles = [];
+        let sucessUploadFiles = [];
+        let failerUploadFiles = [];
+        let allSucessFiles = []
+        const selectFilesName = [];
+
+        const fileUploadPromises = [];
+
+        if (selectFiles.length > 100) {
+
+            const chunkSize = 50;
+            
+
+            for (let i = 0; i < selectFiles.length; i += chunkSize) {
+               
+                const chunkPromises = [];
+
+                for (let j = i; j < Math.min(i + chunkSize, selectFiles.length); j++) {
+                    const fileItem = selectFiles[j];
+                    selectFilesName.push(fileItem.name)
+
+                    if (!selectedSheetData.hasOwnProperty(fileItem.name)) {
+                        missingFiles.push({ "Missing in sheet File Name": fileItem.name });
+                        console.log('file not present in sheet:->', fileItem.name);
+                    } else {
+                        console.log('file present in sheet:->', fileItem.name);
+                        const path = fileItem.uri._fsPath;
+                        const fileReadStream = fs.createReadStream(path);
+                        fileReadStream.fileItem = {
+                            fileAttributes: selectedSheetData[fileItem.name]
+                        };
+
+                        const uploadPromise = new Promise((resolve, reject) => {
+                            
+                                PlatformApi.IafFile.uploadFileResumable(containerObj, fileReadStream, {
+                                    filename: fileItem.name,
+                                    onComplete: (fileItem) => {
+                                        resolve({ "status": "success", "fileName": fileItem.name });
+                                    },
+                                    onError: (error) => {
+                                        reject({ "status": "rejected", "error": error, "fileName": fileItem.name });
+                                    },
+    
+                                }, ctx).catch((error)=>{
+                                    reject({ "status": "rejected", "error": error, "fileName": fileItem.name })
+                                })
+                        });
+
+                        chunkPromises.push(uploadPromise);
+                    }
+                }
+
+                await Promise.allSettled(chunkPromises).then((finalData) => {
+                    finalData.forEach((settledPromise) => {
+                        if (settledPromise.status === 'fulfilled') {
+                            const result = settledPromise.value;
+                            sucessUploadFiles.push({ "Success File Name": result.fileName });
+                            allSucessFiles.push(result.fileName)
+                        } else if (settledPromise.status === 'rejected') {
+                            const reason = settledPromise.reason; 
+                            failerUploadFiles.push({ "Failed File Name": reason.fileName });
+                        }
+                    });
+                });
+            }
+
+        } else {
+            for (const fileItem of selectFiles) {
+                selectFilesName.push(fileItem.name)
+                if (!selectedSheetData.hasOwnProperty(fileItem.name)) {
+                    missingFiles.push({ "Missing File Name": fileItem.name });
+                    console.log('file not present in sheet:->', fileItem.name);
+                } else {
+                    console.log('file present in sheet:->', fileItem.name);
+                    const path = fileItem.uri._fsPath;
+                    const fileReadStream = fs.createReadStream(path);
+                    fileReadStream.fileItem = {
+                        fileAttributes: selectedSheetData[fileItem.name]
+                    };
+                   
+                    const uploadPromise = new Promise((resolve, reject) => {
+                        PlatformApi.IafFile.uploadFileResumable(containerObj, fileReadStream, {
+                            filename: fileItem.name,
+                            onComplete: (fileItem) => {
+                                resolve({ "status": "sucess", "fileName": fileItem.name })
+                            },
+                            onError: (error) => {
+                                reject({ "status": "rejected", "error": error, "fileName": fileItem.name })
+                            }
+                        }, ctx).catch((error)=>{
+                            reject({ "status": "rejected", "error": error, "fileName": fileItem.name })
+                        });
+                    });
+
+                    fileUploadPromises.push(uploadPromise);
+
+                }
+            }
+
+            await Promise.allSettled(fileUploadPromises).then((finalData) => {
+                finalData.forEach((settledPromise) => {
+                    if (settledPromise.status === 'fulfilled') {
+                        const result = settledPromise.value; 
+                        sucessUploadFiles.push({ "Success File Name": result.fileName });
+                        allSucessFiles.push(result.fileName)
+                    } else if (settledPromise.status === 'rejected') {
+                        const reason = settledPromise.reason; 
+                        failerUploadFiles.push({ "Failed File Name": reason.fileName });
+                    }
+                });
+            });
+
+        }
+
+        let missingUpload = [];
+        for(const keys in selectedSheetData){
+            if(!selectFilesName.includes(keys)){
+                missingUpload.push({ "Missing in folder File Name": keys })
+            }
+        }
+
+        let finalFailedFiles = [];
+        for (let i = 0; i < failerUploadFiles.length; i++) {
+            let item = failerUploadFiles[i];
+            let fileItems = await IafScriptEngine.getFileItems(
+                {
+                    collectionDesc: { _userType: iaf_ext_files_coll._userType, _userItemId: iaf_ext_files_coll._userItemId },
+                    query: { "name":  item['Failed File Name'] },
+                    options: { page: { getAllItems: true } }
+                }, ctx);
+            
+            if (fileItems.length == 0) {
+                finalFailedFiles.push(item);
+            }
+            else{
+                if (!allSucessFiles.includes(item['Failed File Name'])) {
+                    console.log('file not include in sucess...')
+                    sucessUploadFiles.push({ "Success File Name": item['Failed File Name'] })
+                }
+                else{
+                    console.log('file pressenttttt in sucessss......')
+                }
+            }
+        }
+
+        if (missingFiles.length > 0) {
+            missingFiles[0].Count = missingFiles.length;
+        }
+        
+        if (sucessUploadFiles.length > 0) {
+            sucessUploadFiles[0].Count = sucessUploadFiles.length;
+        }
+        
+        if (finalFailedFiles.length > 0) {
+            finalFailedFiles[0].Count = finalFailedFiles.length;
+        }
+
+        if (missingUpload.length > 0) {
+            missingUpload[0].Count = missingUpload.length;
+        }
+
+
+        let finalSheetArray = [
+            { sheetName: "Missing_File_In_Sheet", objects: missingFiles },
+            { sheetName: "Successfully_Upload", objects: sucessUploadFiles },
+            { sheetName: "Failed_To_Upload", objects: finalFailedFiles },
+            { sheetName: "Missing_File_In_Folder", objects: missingUpload }
+        ];
+       
+        let relationWorkbook = await UiUtils.IafDataPlugin.createWorkbookFromAoO(finalSheetArray);
+        await UiUtils.IafDataPlugin.saveWorkbook(relationWorkbook, "Bulk_Upload_File_Status.xlsx");
+        return
+    }
+}
+  
+  export default Validation
